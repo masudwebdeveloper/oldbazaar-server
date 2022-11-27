b1429c45b911b1c85afd2fcc6c9f57b00e063dd1
masudrana5 <abdullainfo30@gmail.com>
2022-11-27 15:23:05 +0600

    i start payment gateway system

--- index.js
+++ index.js
@@ -40,7 +40,6 @@ async function run() {
         const bookingsCollections = client.db('OldBazaar').collection('bookings');
         const advertiseCollections = client.db('OldBazaar').collection('advertise');
         // verify admin
-        const verifyAdmin = 
         //for secrect use jwt
         app.get('/jwt', async (req, res) => {
             const email = req.query.email;
@@ -64,9 +63,12 @@ async function run() {
 
         // get all products
         app.get('/products', async (req, res) => {
+            const size = parseInt(req.query.size);
+            const page = parseInt(req.query.page);
             const query = {};
-            const products = await secondHandProductsCollections.find(query).toArray();
-            res.send(products)
+            const products = await secondHandProductsCollections.find(query).skip(page * size).limit(size).toArray();
+            const count = await secondHandProductsCollections.estimatedDocumentCount();
+            res.send({count,products})
         })
 
         //add to product only permision is admin
@@ -110,6 +112,14 @@ async function run() {
             const id = req.params.id;
             const query = {_id: ObjectId(id)};
             const deleteProduct = await reportsCollections.deleteOne(query);
+            res.send(deleteProduct);
+        })
+
+        //delete from advertise collection 
+        app.delete('/advertise/:id', async(req, res)=>{
+            const id = req.params.id;
+            const filter = {productId: id};
+            const deleteProduct = await advertiseCollections.deleteOne(filter);
             res.send(deleteProduct);
         })
         // get only categories
@@ -228,12 +238,20 @@ async function run() {
                 email: productData.email,
                 productName: productData.productName
             }
+            const option = {upsert: true};
+            const filter = {productId: productData.productId};
+            const updateDoc = {
+                $set : {
+                    status: "booked"
+                }
+            }
             const alreadyBooked = await bookingsCollections.find(query).toArray();
             if (alreadyBooked.length) {
                 const message = `You already have been booked ${productData.productName}`
                 return res.send({ acknowledged: false, message })
             }
             const result = await bookingsCollections.insertOne(productData);
+            const update = await advertiseCollections.updateOne(filter, updateDoc, option);
             res.send(result);
         })
 
@@ -248,6 +266,13 @@ async function run() {
             const getBookedProducts = await bookingsCollections.find(query).toArray();
             res.send(getBookedProducts)
 
+        });
+
+        app.get('/bookings/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await bookingsCollections.findOne(query);
+            res.send(result);
         })
 
         //add to advertise ment
@@ -256,12 +281,20 @@ async function run() {
             const query = {
                 productId: productData.productId,
             }
+            const filter = {_id: ObjectId(productData.productId)};
+            const options = {upsert: true};
+            const updateDoc = {
+                $set:{
+                    isAdvertise: true
+                }
+            }
             const alreadyAdvertise = await advertiseCollections.find(query).toArray();
             if(alreadyAdvertise.length){
                 const message = 'This product already advertise is running';
                 return res.send({acknowledged: false, message})
             }
             const addProduct = await advertiseCollections.insertOne(productData);
+            const advertiseProduct = await secondHandProductsCollections.updateOne(filter, updateDoc, options);
             res.send(addProduct);
         })
 
